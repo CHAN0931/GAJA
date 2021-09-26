8548cd680ab52f005d8f039cafa51740111234c6
CHAN0931 <ashrky143@gmail.com>
2021-09-26 21:21:28 +0900

    floating button

--- app/src/main/java/com/example/gaja/ui/Location.kt
+++ app/src/main/java/com/example/gaja/ui/Location.kt
@@ -1,29 +1,19 @@
 package com.example.gaja.ui
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
 import com.example.gaja.R
 import com.naver.maps.map.*
 import com.naver.maps.map.util.FusedLocationSource
-import kotlinx.android.synthetic.main.fragment_location.*
-import java.util.jar.Manifest
 
 class location : Fragment(), OnMapReadyCallback {
     var TAG:String = "로그"
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -37,13 +27,13 @@ class location : Fragment(), OnMapReadyCallback {
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         return inflater.inflate(R.layout.fragment_location, container, false)
     }
+
     override fun onMapReady(naverMap: NaverMap) { //맵활성화
         Log.d(TAG, "MainActivity - onMapReady")
         this.naverMap = naverMap
         naverMap.locationSource = locationSource
         naverMap.uiSettings.isLocationButtonEnabled = true
     }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
